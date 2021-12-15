cea1a464a7f57351719b046fee3926b82f2c7493
Luiz Carlos Vilela <41112779+filipezaidan@users.noreply.github.com>
2021-12-15 17:49:23 -0300

    add

--- src/components/DevInformation/DevInformation.tsx
+++ src/components/DevInformation/DevInformation.tsx
@@ -1,10 +1,20 @@
 import * as C from './styles';
-
+import profile from '../../assets/profile.jpg';
 
 const DevInformation = () => {
     return(
         <C.Container>
-            
+            <C.ImageContainer>
+                <C.ImageProfile src={profile}/>
+
+            </C.ImageContainer>
+            <C.ProfileContainer>
+                <C.TitleContainer>
+                    <C.Name>Filipe Zaidan</C.Name>
+
+                </C.TitleContainer>
+
+            </C.ProfileContainer>
         </C.Container>
     );
 }
--- src/components/DevInformation/styles.ts
+++ src/components/DevInformation/styles.ts
@@ -4,7 +4,55 @@ import styled from 'styled-components';
 export const Container = styled.div`
     display: flex;
     width: 100%;
-    height: 70%;
+    height: 75%;
     background-color: #1F2A48;
     border-radius: 10px;
+    padding: 6%;
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
+
+export const ImageContainer = styled.div`
+    /* background: red; */
+    width: 25%;
+    height: 100%;
+    padding: 2%;
+`;
+
+export const ProfileContainer = styled.div`
+    /* background: green; */
+    width: 70%;
+    height: 100%;
+    padding: 2%;
+`;
+
+export const ImageProfile = styled.img`
+display: inline-block;
+    width: 120px;
+    height: 120px;
+    border-radius: 50%;
+    background-color: #FFF;
+    object-fit: cover;
+`;
+
+export const TitleContainer = styled.div`
+    display: flex;
+    width: 100%;
+    height: 15%;
+    flex-direction: row;
+    justify-content: space-between;
+    /* background-color: #FFF; */
+`;
+
+export const Name = styled.p`
+    font-size: 22px;
+    font-family: 'Nunito Sans', sans-serif;
+    font-weight: bold;
+    color: #FFF;
+`;
+
+export const CreateAccount= styled.p`
+    font-size: 14px;
+    color: #d9d9d9
 `;
